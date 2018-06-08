8db16833d2fc31d56264ad4d85c266bd1c7ae3c2
Jeremy Bunting <buntingj@gmail.com>
2018-06-08 09:32:57 -0700

    added phil's changes adding aggressive reconnect settings for the mqtt client added changed logging

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const mqtt = require('mqtt')
 const mClient = mqtt.connect(process.env.MQTT_BROKER_ADDRESS, {
     username: process.env.MQTT_USER,
     password: process.env.MQTT_PASSWORD,
+    keepalive: 10000,
+    connectTimeout: 120000,
+    reconnectPeriod: 500
 })
 
 const api = new AmbientWeatherApi({
@@ -12,29 +15,54 @@ const api = new AmbientWeatherApi({
 })
 
 let ts = () => new Date()
-const conn = process.env.MQTT_CONNECT_TOPIC | 'aw/connect'
-const subscribe = process.env.MQTT_SUBSCRIBE_TOPIC | 'aw/subscribe'
-const update = process.env.MQTT_UPDATE_TOPIC | 'aw/update'
 
-api.connect()
+const handleClientError = function(e){
+    console.log('connection error to broker, exiting')
+    setTimeout(()=>{
+        process.exit()
+    }, 10000)
+}
+
+mClient.on('error', handleClientError)
+
+mClient.on('offline', function () {
+    console.log(`${ts()} - BROKER OFFLINE`)
+})
+
+let publishHandler = function (e) {
+    if (e) {
+        return console.error(e)
+    }
+    console.log(`${ts()} - mqtt publish`)
+}
+
+// connect to cloud once mqtt is up:
+mClient.on('connect', function () {
+    console.log(`${ts()} - mqtt connected`)
+    mClient.publish('/aw/alive', null, publishHandler)
+})
+
 
 api.on('error', e => {
     console.error('shit is sideways:', e)
 })
 
 api.on('connect', () => {
-    console.log(`connected: ${ts()}`)
-    mClient.publish(conn)
+    console.log(`${ts()} - connected`)
+    mClient.publish('/aw/connect', null, publishHandler)
+    api.subscribe(process.env.API_KEY)
 })
 
 api.on('subscribed', data => {
-    console.log(`subscribed: ${ts()}`)
-    mClient.publish(subscribe, JSON.stringify(data))
+    console.log(`${ts()} - subscribed`)
+    mClient.publish('/aw/subscribe', JSON.stringify(data))
+    // publish lastdata as data to avoid update interval blackout:
+    mClient.publish('/aw/data', JSON.stringify(data['devices'][0]['lastData']), publishHandler)
 })
 
 api.on('data', data => {
-    console.log(`data: ${ts()}`)
-    mClient.publish(update, JSON.stringify(data))
+    console.log(`${ts()} - aw data`)
+    mClient.publish('/aw/data', JSON.stringify(data), publishHandler)
 })
 
-api.subscribe(process.env.API_KEY)
+api.connect()
