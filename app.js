e6dceadbb9c0dbbba7c2337a4d1461474738d0b6
Bill Church <wmchurch@gmail.com>
2018-09-14 21:32:22 -0400

    Convert JSON data to individual MQTT topics
    Instead of publishing a monolithic JSON data topic, break out the individual properties to topics with their values.
    
    Do this programmatically as to not require constant upkeep if the json changes
    
    Also change "shit is sideways" to "API error" ;)

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ let publishHandler = function (e) {
     if (e) {
         return console.error(e)
     }
-    console.log(`${ts()} - mqtt publish`)
+    // console.log(`${ts()} - mqtt publish`)
 }
 
 // connect to cloud once mqtt is up:
@@ -44,7 +44,7 @@ mClient.on('connect', function () {
 
 
 api.on('error', e => {
-    console.error('shit is sideways:', e)
+    console.error('API Error:', e)
 })
 
 api.on('connect', () => {
@@ -58,6 +58,7 @@ api.on('subscribed', data => {
     mClient.publish('aw/subscribe', JSON.stringify(data))
     // publish lastdata as data to avoid update interval blackout:
     var lastData = data['devices'][0]['lastData']
+    // loop through properties and publish each one as it's own topic
     for (var prop in lastData) {
         if (lastData.hasOwnProperty(prop)) {
             mClient.publish('aw/data/' + prop, JSON.stringify(lastData[prop]), publishHandler)
@@ -67,8 +68,9 @@ api.on('subscribed', data => {
 
 api.on('data', data => {
     console.log(`${ts()} - aw data`)
-    // delete device node to prevent spamming
+    // delete device node to prevent topic spamming
     delete data.device
+    // loop through properties and publish each one as it's own topic
     for (var prop in data) {
         if (data.hasOwnProperty(prop)) {
             mClient.publish('aw/data/' + prop, JSON.stringify(data[prop]), publishHandler)
